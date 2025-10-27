7060d882c985fb2f0bdf0a109c186311d5566bcc
Kawsar Ahmed <kawsarahmed@Kawsars-MacBook-Air.local>
2025-10-27 19:19:17 -0400

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import { Navbar } from "@/components/Navbar";
+import { StarBackground } from "@/components/StarBackground";
+import { HeroSection } from "../components/HeroSection";
+import { ThemeToggle } from "../components/ThemeToggle";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+
+      {/* Background Effects */}
+      <StarBackground />
+
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Main Content */}
+      <main>
+        <HeroSection />
+      </main>
+
+      {/* Footer */}
+    </div>
+  );
+};
